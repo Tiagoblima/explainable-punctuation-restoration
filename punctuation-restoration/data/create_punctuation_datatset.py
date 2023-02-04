29ae9ba527722c85ea33fe918a0129df5d3ea844
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-04 16:40:01 -0300

    adding create dataset

--- punctuation-restoration/data/create_punctuation_datatset.py
+++ punctuation-restoration/data/create_punctuation_datatset.py
@@ -3,12 +3,12 @@ import re
 
 import click
 from datasets import load_dataset
-from utils.utils import text2labels
+from utils.utils import text2labels, text2t5labels
 
 import jsonlines
 
 
-def build_dataset(dataset, save_path):
+def build_dataset(dataset, save_path, data_format):
     for split in dataset.column_names:
 
         data = dataset[split]['text']
@@ -27,8 +27,12 @@ def build_dataset(dataset, save_path):
 
                 item = {"text_id": i}
 
-                item.update(text2labels(text))
-
+                if data_format == "bert":
+                    item.update(text2labels(text))
+                elif data_format == "t5":
+                    item.update(text2t5labels(text))
+                else:
+                    raise ValueError("Unknown data format")
                 i += 1
                 f.write(item)
 
@@ -37,18 +41,22 @@ def build_dataset(dataset, save_path):
 @click.option('--path_to_save', type=str, default='./punctuation-restoration/data')
 @click.option('--dataset_name', type=str, default='tiagoblima/tedtalk2012-03')
 @click.option('--save_dataset', type=str, default='tiagoblima/punctuation-tedtalk2012')
+@click.option('--data_format', type=str, default="t5", help="bert or t5", )
 @click.option('--private', type=bool, default=True)
 @click.option('--use_auth_token', type=bool, default=True)
-def main(path_to_save, dataset_name, save_dataset, private=True, use_auth_token=True):
+def main(path_to_save, dataset_name, save_dataset, data_format, private=True, use_auth_token=True):
     os.makedirs(path_to_save, exist_ok=True)
 
     dataset = load_dataset(dataset_name, use_auth_token=use_auth_token)
 
-    build_dataset(dataset, path_to_save)
+    build_dataset(dataset, path_to_save, data_format)
 
     new_dataset = load_dataset('json', data_files={'train': os.path.join(path_to_save, 'train.jsonl'),
                                                    'validation': os.path.join(path_to_save, 'validation.jsonl'),
                                                    'test': os.path.join(path_to_save, 'test.jsonl')})
+    if save_dataset.split('-')[-1] != data_format:
+        save_dataset = save_dataset + '-' + data_format
+
     new_dataset.push_to_hub(save_dataset, private=private)
 
 
