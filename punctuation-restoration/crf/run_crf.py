13754bb6c37910de5321382a7c054f79d9c90bee
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-20 11:19:50 -0300

    convert to ascii

--- punctuation-restoration/crf/run_crf.py
+++ punctuation-restoration/crf/run_crf.py
@@ -139,9 +139,9 @@ def run(args):
 
         crf = CRF(algorithm='lbfgs', c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
 
-        print('\nEvaluating CRF')
-
+        print("Training CRF")
         crf.fit(X_train, y_train)
+        print('\nEvaluating CRF')
 
         y_pred = crf.predict(X_test)
         print(y_pred[:1])
