872d05a0c94ccc34a46d5c1223d429d548184140
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-22 20:55:45 -0300

    evaluate model crf

--- punctuation-restoration/crf/evaluate_crf.py
+++ punctuation-restoration/crf/evaluate_crf.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import pickle
 
 from seqeval.metrics import classification_report
@@ -7,19 +8,15 @@ from utils import read_corpus_file, data_preprocessing, convert_data, dump_repor
 
 parser = argparse.ArgumentParser(description='Process dataframe data.')
 
-def main(model_file, path_to_data, report_dir, report_file, args):
-    test_data = read_corpus_file(test_file, split_char=',')
-    # Load dataset
-    test_data = data_preprocessing(test_data)
 
-    X_train, y_train = convert_data(train_data)
-    X_test, y_test = convert_data(test_data)
-
-    crf = pickle.load(open(model_file, 'rb'))
-    y_pred = crf.predict(X_test)
-
+def evaluate(x_test, y_test, model_path):
+    crf = pickle.load(open(model_path, 'rb'))
+    y_pred = crf.predict(x_test)
     dict_report = classification_report(y_test, y_pred, output_dict=True)
+    return dict_report, y_pred
 
+
+def build_report(test_data, y_test, y_pred):
     data_conll = ''
 
     for data, real_tags, pred_tags in zip(test_data, y_test, y_pred):
@@ -28,21 +25,36 @@ def main(model_file, path_to_data, report_dir, report_file, args):
                          for word, real_tag, pred_tag in zip(words, real_tags, pred_tags))
         sent += '\n\n'
         data_conll += sent
+    return data_conll
+
+
+def main(args):
+    test_file = os.path.join(args.path_to_data, 'test.csv')
+    test_data = read_corpus_file(test_file, split_char=',')
+    # Load dataset
+    test_data = data_preprocessing(test_data)
+    X_test, y_test = convert_data(test_data)
+
+    dict_report, y_pred = evaluate(X_test, y_test, args.model_path)
+
+    os.makedirs(args.report_path, exist_ok=True)
+    args.report_path = os.path.join(args.report_path, os.path.split(args.path_to_data)[-1])
 
     print('\nReport:', dict_report)
 
-    print('\nSaving the report in:', report_file)
+    print('\nSaving the report in:', args.report_path)
 
-    dump_report(dict_report, report_file)
+    dump_report(dict_report, os.path.join(args.report_path, 'report.csv'))
 
-    script_result_file = os.path.join(report_dir, args.corpus_name + '_crf.tsv')
+    script_result_file = os.path.join(args.report_path, 'data_conll.tsv')
 
+    data_conll = build_report(test_data, y_test, y_pred)
     with open(script_result_file, 'w', encoding='utf-8') as file:
         file.write(data_conll)
 
 
 if __name__ == '__main__':
-    parser.add_argument('--result_path',
+    parser.add_argument('--model_path',
                         default='./results/',
                         help='output filename')
 
@@ -50,10 +62,8 @@ if __name__ == '__main__':
                         default='./data/tedtalk2012',
                         help='Files must be a dataframe with headers sentence_id,words,label')
 
-    parser.add_argument('--k_fold_eval',
-                        action='store_true',
-                        default=False,
-                        help='Files must be a dataframe with headers sentence_id,words,label')
-
+    parser.add_argument('--report_path',
+                        default='./reports/',
+                        help='Path to final report')
     args = parser.parse_args()
-    run(args)
+    main(args)
