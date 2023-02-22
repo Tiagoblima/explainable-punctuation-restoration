10fabc49b63906ad9625dfa052890c936495ff47
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-22 07:16:44 -0300

    continue instead of raise valueerror

--- punctuation-restoration/crf/evaluate_crf.py
+++ punctuation-restoration/crf/evaluate_crf.py
@@ -1,64 +1,25 @@
 import argparse
-import os
 import pickle
-from itertools import chain
 
-import pandas as pd
 from seqeval.metrics import classification_report
-from sklearn_crfsuite import CRF
 
 from utils import read_corpus_file, data_preprocessing, convert_data, dump_report
 
 parser = argparse.ArgumentParser(description='Process dataframe data.')
 
-
-def run(args):
-    args.corpus_name = args.path_to_data.split('/')[-1]
-
-    report_dir = f'./results/{args.corpus_name}'
-    os.makedirs(report_dir, exist_ok=True)
-
-    train_file = os.path.join(args.path_to_data, 'train.csv')
-    test_file = os.path.join(args.path_to_data, 'test.csv')
-
-    report_file = os.path.join(report_dir, args.corpus_name + '_crf.csv')
-
+def main(model_file, path_to_data, report_dir, report_file, args):
     test_data = read_corpus_file(test_file, split_char=',')
-    train_data = read_corpus_file(train_file, split_char=',')
-
-    print('\n  Train data:', len(train_data))
-    print('  Test data:', len(test_data))
-
-    print('\nPreprocessing ...')
-
-    print('\n  Train data')
-
-    train_data = data_preprocessing(train_data)
-
-    print('  Test data')
-
+    # Load dataset
     test_data = data_preprocessing(test_data)
 
     X_train, y_train = convert_data(train_data)
     X_test, y_test = convert_data(test_data)
-    pd.DataFrame.from_dict(X_train[0]).T.to_csv(f'{args.corpus_name}_X_train.csv', index=False)
-    print('\nExample features:', X_train[0])
-    print('Tags:', y_train[0])
 
-    crf = CRF(algorithm='lbfgs', c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
-
-    print("Training CRF")
-    crf.fit(X_train, y_train)
-    print('\nEvaluating CRF')
-
+    crf = pickle.load(open(model_file, 'rb'))
     y_pred = crf.predict(X_test)
-    print(y_pred[:1])
-    print(y_test[:1])
-    print(set(chain.from_iterable(y_pred)))
-    print(set(chain.from_iterable(y_test)))
+
     dict_report = classification_report(y_test, y_pred, output_dict=True)
 
-    pickle.dump(crf, open(args.model_type, 'wb'))
     data_conll = ''
 
     for data, real_tags, pred_tags in zip(test_data, y_test, y_pred):
