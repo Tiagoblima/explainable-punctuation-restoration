36b558533e8c28f842a7a928cb4f96214e0da3bd
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-23 07:59:54 -0300

    change filename

--- punctuation-restoration/blistm/evaluate.py
+++ punctuation-restoration/blistm/evaluate.py
@@ -12,10 +12,7 @@ def evaluate(corpus, tagger: SequenceTagger):
     print(result.detailed_results)
     clf_report = result.classification_report
 
-    clf_report.pop('weighted avg')
-    clf_report.pop('macro avg')
-
-    return clf_report['micro avg'], clf_report
+    return clf_report
 
 
 @click.command()
@@ -35,8 +32,8 @@ def main(
     model = SequenceTagger.load(path_to_model)
     clf_report, report = evaluate(corpus, model)
     os.makedirs(report_path, exist_ok=True)
-    pd.DataFrame(report, index=[0]).to_csv(os.path.join(report_path, 'report.csv'), index=False)
-    pd.DataFrame(clf_report, index=[0]).to_csv(os.path.join(report_path, 'clf_report.csv'), index=False)
+    pd.DataFrame(report).T.to_csv(os.path.join(report_path, 'report.csv'))
+    pd.DataFrame(clf_report).T.to_csv(os.path.join(report_path, 'clf_report.csv'))
 
     print(clf_report)
     print(report)
