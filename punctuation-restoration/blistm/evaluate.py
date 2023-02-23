26fde52b2278e08f1f9267150c5cd32f40a1590c
Tiago de Lima <tiago.blima02@gmail.com>
2023-02-23 07:16:23 -0300

    change filename

--- punctuation-restoration/blistm/evaluate.py
+++ punctuation-restoration/blistm/evaluate.py
@@ -1,10 +1,12 @@
+import json
+import os
+
+import click
 from flair.datasets import ColumnCorpus
 from flair.models import SequenceTagger
 
 
-def evaluate(corpus, path_to_model='./tedtalk2012/best-model.pt'):
-
-    tagger: SequenceTagger = SequenceTagger.load(path_to_model)
+def evaluate(corpus, tagger: SequenceTagger):
     result = tagger.evaluate(corpus.test, gold_label_type='ner')
     print(result.detailed_results)
     clf_report = result.classification_report
@@ -13,3 +15,30 @@ def evaluate(corpus, path_to_model='./tedtalk2012/best-model.pt'):
     clf_report.pop('macro avg')
 
     return clf_report['micro avg'], clf_report
+
+
+@click.command()
+@click.option('--path_to_model', default='./results/bilstm-model.pt', help='Path to model')
+@click.option('--path_to_data', default='./data/tedtalk2012', help='Path to data')
+@click.option('--report_path', default='./results/', help='Path to final report')
+def main(
+        path_to_model: str,
+        path_to_data: str,
+        report_path: str
+):
+
+    columns = {0: 'text', 1: 'ner'}
+    corpus: ColumnCorpus = ColumnCorpus(path_to_data, columns,
+                                        train_file='train.csv',
+                                        test_file='test.csv',
+                                        dev_file='dev.csv')
+    model = SequenceTagger.load(path_to_model)
+    clf_report, report = evaluate(corpus, model)
+    os.makedirs(report_path, exist_ok=True)
+    json.dump(report, open(os.path.join(report_path, 'report.json'), 'w'))
+    json.dump(clf_report, open(os.path.join(report_path, 'clf_report.json'), 'w'))
+    print(clf_report)
+    print(report)
+
+if __name__ == '__main__':
+    main()
